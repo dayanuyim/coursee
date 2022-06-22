f8777b6fa6428c08bed4e3af3ead8ac28d6c79b2
dayanuyim <dayanuyim@gmail.com>
2022-06-23 00:19:17 +0800

    Rename Data Folders
    YYYY-MM-DD-<Name>

---
+++ app/data/YYYY-MM-DD-郡大西巒/course.md
@@ -0,0 +1,27 @@
+# 郡大西巒(2022-08-21) #
+
+## 待辦事項 ##
+
+- MM/DD(W)
+    - [ ] 留守
+    - [ ] 保險
+    - [X] 確認路況
+    - [ ] 充電(電池/營燈)
+
+- MM/DD(W)
+    - [ ] 糧食/炊具
+    - [X] 火車票
+    - [ ] 打包完成(手電筒/營燈/拖鞋/盥洗用品)
+
+- MM/DD(W)
+    - [ ] 剪指甲
+
+
+## 天氣預報 ##
+
+- 日出日落 [0625~1754](https://sunrise.maplogs.com/zh-TW/taiwan.777.html)
+- 中央氣象局 [佳暮](http://www.cwb.gov.tw/V7/forecast/entertainment/tribes/014_1001327A04.htm) | [知本](http://www.cwb.gov.tw/m/f/entertainment/E040.php)
+
+## 文件 ##
+
+
--- app/trek-info.js
+++ app/trek-info.js
@@ -1,7 +1,21 @@
 export default
 [
     {
-      "filename": "2019-0228-守城有勝O型",
+      "title": "郡大西巒",
+      "date": "YYYY-MM-DD",
+      "days": "5",
+      "facebook": null,
+      "keepon": null,
+      "hiking_biji": null,
+    },
+    {
+      "title": "中央山脈中段：室呂木、干卓萬、七彩湖",
+      "date": "YYYY-MM-DD",
+      "days": 1,
+      "facebook": null,
+      "keepon": null
+    },
+    {
       "title": "守城有勝O型",
       "date": "2019-02-28",
       "days": "3",
@@ -9,7 +23,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2019-0217-坪林火炎山脈",
       "title": "坪林火炎山脈",
       "date": "2019-02-17",
       "days": "1",
@@ -17,7 +30,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2019-0112-拔西猴山、姜子寮山、淡蘭古道暖東段",
       "title": "拔西猴山、姜子寮山、淡蘭古道暖東段",
       "date": "2019-01-12",
       "days": "1",
@@ -25,7 +37,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2018-1006-南湖北稜",
       "title": "南湖北稜",
       "date": "2018-10-06",
       "days": "5",
@@ -33,7 +44,6 @@ export default
       "keepon": "https://www.keepon.com.tw/thread-eb566224-0dd1-e811-80dc-901b0e54a4e6.html"
     },
     {
-      "filename": "2018-0404-扁柏神殿",
       "title": "扁柏神殿",
       "date": "2018-04-04",
       "days": "3",
@@ -41,7 +51,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2018-0217-大小鬼湖",
       "title": "大小鬼湖",
       "date": "2018-02-17",
       "days": "10",
@@ -49,7 +58,6 @@ export default
       "keepon": "https://www.keepon.com.tw/thread-178894b0-38db-e811-80dc-901b0e54a4e6.html"
     },
     {
-      "filename": "2017-0529-大屯溪古道、小觀音山西峰",
       "title": "大屯溪古道、小觀音山西峰",
       "date": "2017-05-29",
       "days": 1,
@@ -57,7 +65,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2017-0304-八連溪古道",
       "title": "八連溪古道",
       "date": "2017-03-04",
       "days": 1,
@@ -65,7 +72,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2017-0212-淡水無極天元宮、向天山",
       "title": "淡水無極天元宮、向天山",
       "date": "2017-02-12",
       "days": 1,
@@ -73,7 +79,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2017-0130-嵐山工作站",
       "title": "嵐山工作站",
       "date": "2017-01-30",
       "days": "5",
@@ -81,7 +86,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2017-0101-中正山、大屯西峰",
       "title": "中正山、大屯西峰",
       "date": "2017-01-01",
       "days": 1,
@@ -89,7 +93,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-1204-大屯溪古道、竿尾崙",
       "title": "大屯溪古道、竿尾崙",
       "date": "2016-12-04",
       "days": 1,
@@ -97,7 +100,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-1112-灣潭古道、北勢溪古道",
       "title": "灣潭古道、北勢溪古道",
       "date": "2016-11-12",
       "days": 1,
@@ -105,7 +107,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-1104-多望溪-加羅神社",
       "title": "多望溪-加羅神社",
       "date": "2016-11-04",
       "days": "3",
@@ -113,7 +114,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0924-西勢坑古道、暖東峽谷",
       "title": "西勢坑古道、暖東峽谷",
       "date": "2016-09-24",
       "days": 1,
@@ -121,7 +121,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0813-木炭古道",
       "title": "木炭古道",
       "date": "2016-08-13",
       "days": "2",
@@ -129,7 +128,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0730-羅馬縱走",
       "title": "羅馬縱走",
       "date": "2016-07-30",
       "days": "2",
@@ -137,7 +135,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0717-三貂嶺瀑布群、五分寮山",
       "title": "三貂嶺瀑布群、五分寮山",
       "date": "2016-07-17",
       "days": 1,
@@ -145,7 +142,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0703-姆指山、劍頭山、糶米古道",
       "title": "姆指山、劍頭山、糶米古道",
       "date": "2016-07-03",
       "days": 1,
@@ -153,7 +149,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0626-西勢水庫、暖東峽谷",
       "title": "西勢水庫、暖東峽谷",
       "date": "2016-06-26",
       "days": 1,
@@ -161,7 +156,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0619-炮子崙瀑布、茶山古道",
       "title": "炮子崙瀑布、茶山古道",
       "date": "2016-06-19",
       "days": 1,
@@ -169,7 +163,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0612-南邦寮古道、尾寮古道南段",
       "title": "南邦寮古道、尾寮古道南段",
       "date": "2016-06-12",
       "days": 1,
@@ -177,7 +170,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0612-土庫岳",
       "title": "土庫岳",
       "date": "2016-06-12",
       "days": 1,
@@ -185,7 +177,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0610-南港後山",
       "title": "南港後山",
       "date": "2016-06-10",
       "days": 1,
@@ -193,7 +184,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0605-內雙溪古道、瑪礁古道",
       "title": "內雙溪古道、瑪礁古道",
       "date": "2016-06-05",
       "days": 1,
@@ -201,7 +191,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0529-麻竹寮山",
       "title": "麻竹寮山",
       "date": "2016-05-29",
       "days": 1,
@@ -209,7 +198,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0515-新山夢湖",
       "title": "新山夢湖",
       "date": "2016-05-15",
       "days": 1,
@@ -217,7 +205,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0430-香菇橋上平岩山縱走多加屯山",
       "title": "香菇橋上平岩山縱走多加屯山",
       "date": "2016-04-30",
       "days": "3",
@@ -225,7 +212,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0402-佐得寒、塔雅府、桃山",
       "title": "佐得寒、塔雅府、桃山",
       "date": "2016-04-02",
       "days": "3",
@@ -233,7 +219,6 @@ export default
       "keepon": "http://www.keepon.com.tw/thread-bb758562-a1ff-e511-80c1-901b0e54a4e6.html"
     },
     {
-      "filename": "2016-0327-龍船岩、白石湖山、大崙尾山",
       "title": "龍船岩、白石湖山、大崙尾山",
       "date": "2016-03-27",
       "days": 1,
@@ -241,7 +226,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0320-鯉魚山、龍船岩",
       "title": "鯉魚山、龍船岩",
       "date": "2016-03-20",
       "days": 1,
@@ -249,7 +233,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0313-象山、南港山",
       "title": "象山、南港山",
       "date": "2016-03-13",
       "days": 1,
@@ -257,7 +240,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2016-0228-鳶山縱走",
       "title": "鳶山縱走",
       "date": "2016-02-28",
       "days": 1,
@@ -265,7 +247,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2015-1101-雪山西稜",
       "title": "雪山西稜",
       "date": "2015-11-01",
       "days": "7",
@@ -273,7 +254,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2015-0905-小腳ㄚ會師-合歡主北線",
       "title": "小腳ㄚ會師-合歡主北線",
       "date": "2015-09-05",
       "days": "2",
@@ -281,7 +261,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2015-0704-福山溯札孔溪2K",
       "title": "福山溯札孔溪2K",
       "date": "2015-07-04",
       "days": 1,
@@ -289,7 +268,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2015-0501-神魔縱走南湖北山",
       "title": "神魔縱走南湖北山",
       "date": "2015-05-01",
       "days": "3",
@@ -297,7 +275,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2015-0403-雪白山縱走唐穗山",
       "title": "雪白山縱走唐穗山",
       "date": "2015-04-03",
       "days": "4",
@@ -305,7 +282,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2015-0101-鎮金邊",
       "title": "鎮金邊",
       "date": "2015-01-01",
       "days": "4",
@@ -313,7 +289,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-1005-能高安東軍",
       "title": "能高安東軍",
       "date": "2014-10-05",
       "days": "6",
@@ -321,7 +296,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0906-神魔塚(未盡)",
       "title": "神魔塚(未盡)",
       "date": "2014-09-06",
       "days": "3",
@@ -329,7 +303,6 @@ export default
       "keepon": "https://www.keepon.com.tw/thread-fd71d6d2-1ad8-e411-93ec-000e04b74954.html"
     },
     {
-      "filename": "2014-0829-模拳松",
       "title": "模拳松",
       "date": "2014-08-29",
       "days": "3",
@@ -337,7 +310,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0802-東模拳松",
       "title": "東模拳松",
       "date": "2014-08-02",
       "days": "2",
@@ -345,7 +317,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0712-蕃社跡山(安全下溪路)",
       "title": "蕃社跡山(安全下溪路)",
       "date": "2014-07-12",
       "days": "2",
@@ -353,7 +324,6 @@ export default
       "keepon": "https://www.keepon.com.tw/thread-44390ba3-1ad8-e411-93ec-000e04b74954.html"
     },
     {
-      "filename": "2014-0501-戒茂斯上嘉明妹池",
       "title": "戒茂斯上嘉明妹池",
       "date": "2014-05-01",
       "days": "4",
@@ -361,7 +331,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0315-司馬庫斯古道上雪白出唐穗",
       "title": "司馬庫斯古道上雪白出唐穗",
       "date": "2014-03-15",
       "days": "2",
@@ -369,7 +338,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0228-雪白、鴛鴦湖、唐穗",
       "title": "雪白、鴛鴦湖、唐穗",
       "date": "2014-02-28",
       "days": "3",
@@ -377,7 +345,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0215-舊武塔國小",
       "title": "舊武塔國小",
       "date": "2014-02-15",
       "days": "2",
@@ -385,7 +352,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0125-向天湖縱走加里山、出紅毛山",
       "title": "向天湖縱走加里山、出紅毛山",
       "date": "2014-01-25",
       "days": "2",
@@ -393,7 +359,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0118-加里山下風美溪",
       "title": "加里山下風美溪",
       "date": "2014-01-18",
       "days": "2",
@@ -401,7 +366,6 @@ export default
       "keepon": null
     },
     {
-      "filename": "2014-0104-巫山東麓",
       "title": "巫山東麓",
       "date": "2014-01-04",
       "days": 1,
@@ -409,19 +373,10 @@ export default
       "keepon": null
     },
     {
-      "filename": "2013-0126-拉樸山、波露山、阿玉山",
       "title": "拉樸山、波露山、阿玉山",
       "date": "2013-01-26",
       "days": "2",
       "facebook": null,
       "keepon": null
     },
-    {
-      "filename": "plan-01-中央山脈中段：室呂木、干卓萬、七彩湖",
-      "title": "中央山脈中段：室呂木、干卓萬、七彩湖",
-      "date": "plan-01",
-      "days": 1,
-      "facebook": null,
-      "keepon": null
-    }
 ]
