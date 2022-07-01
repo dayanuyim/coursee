f954eb05cf12a7809ef9b32f3d09c8ac0d276f1c
dayanuyim <dayanuyim@gmail.com>
2022-07-01 10:34:24 +0800

    Create data.sh to Generate data.json
    deleted:    app/data.json
    new file:   app/data.sh

---
+++ app/data.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+dir="${0%/*}/data"
+
+function parse()
+{
+    # date prefix
+    date="${1:0:10}"
+
+    if [[ ${#date} != 10 || ${date:4:1} != '-' || ${date:7:1} != '-' ]]; then
+        >&2 echo "Bad Date Format: $1, $date"
+        return 1
+    fi
+
+    # strip suffix days
+    title="${1:11}"
+    title="${title%-[1-9]}"
+    title="${title%-[1-9][0-9]}"
+
+    # suffix days
+    pos=$((11+${#title}+1))
+    days="${1:$pos}"
+    #days="${days:-1}"
+
+    echo -n "{\"date\": \"$date\", \"days\": \"$days\", \"title\": \"$title\"}"
+}
+
+function data(){
+    find "$dir" -maxdepth 1 -type d -iname 'YYYY-*' -or -iname '20*' | sort -r
+}
+
+
+echo "["
+
+line=
+IFS=$'\n'
+
+# print the prev line
+for path in $(data); do
+    if [[ -n $line ]]; then
+        echo "  $line,"
+    fi
+    line="$(parse "${path##*/}")"
+done
+
+# print the last line
+if [[ -n $line ]]; then
+    echo "  $line"
+fi
+
+echo "]"
+
